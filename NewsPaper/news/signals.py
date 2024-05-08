9243912a205d038a7b3da45aacd13a354c1e63a6
AmmaReya <simankova.anastasiya.r@gmail.com>
2024-05-08 22:35:29 +0300

    Added subscriptions, mailing

---
+++ NewsPaper/news/signals.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from django.core.mail import EmailMultiAlternatives
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+
+from .models import PostCategory
+
+
+@receiver(m2m_changed, sender=PostCategory)
+def post_created(instance, sender, **kwargs):
+    if kwargs['action'] == 'post_add':
+        subscribers_email = []
+        for category in instance.post_category.all():
+            subscribers_email += User.objects.filter(subscriptions__category=category).values_list('email', flat=True)
+            subject = f'Новый пост в твоей любимой категории'
+            text_content = (
+                f'Пост: {instance.title}\n'
+                f'Автор: {instance.author.author.username}\n\n'
+                f'Читать больше: http://127.0.0.1:8000{instance.get_absolute_url()}'
+            )
+            html_content = (
+                f'Пост: {instance.title}<br>'
+                f'Автор: {instance.author.author.username}<br><br>'
+                f'<a href="http://127.0.0.1:8000{instance.get_absolute_url()}">'
+                f'Читать больше </a>'
+            )
+            for email in subscribers_email:
+                msg = EmailMultiAlternatives(subject, text_content, None, [email])
+                msg.attach_alternative(html_content, 'text/html')
+                msg.send()
---
+++ NewsPaper/news/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import (PostList, PostDetail, PostSearch, PostCreate, PostUpdate, PostDelete, subscriptions)
+
+
+urlpatterns = [
+    path('', PostList.as_view(), name='post_list'),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('search/', PostSearch.as_view(), name='post_search'),
+
+    path('news/create/', PostCreate.as_view(), name='new_create'),
+    path('news/<int:pk>/update/', PostUpdate.as_view(), name='new_update'),
+    path('news/<int:pk>/delete/', PostDelete.as_view(), name='new_delete'),
+
+    path('articles/create/', PostCreate.as_view(), name='article_create'),
+    path('articles/<int:pk>/update/', PostUpdate.as_view(), name='article_update'),
+    path('articles/<int:pk>/delete/', PostDelete.as_view(), name='article_delete'),
+
+    path('subscriptions/', subscriptions, name='subscriptions')
+]
